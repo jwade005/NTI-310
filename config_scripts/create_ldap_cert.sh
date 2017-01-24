c2c4bee475e01b2e1fba9ab5b9caaa0b16c33e9b
Jonathan Wade <jwade005@seattlecentral.edu>
2017-01-23 16:07:34 -0800

    Update create_ldap_cert.sh

--- config_scripts/create_ldap_cert.sh
+++ config_scripts/create_ldap_cert.sh
@@ -1,49 +1,11 @@
 #!/bin/bash
- 
-#Required
-domain=$1
-commonname=$domain
- 
-#Change to your company details
-country=US
-state=Washington
-locality=Seattle
-organization=jwade.local
-organizationalunit=nti310
-email=jwade005@seattlecentral.edu
 
- 
-if [ -z "$domain" ]
-then
-    echo "Argument not present."
-    echo "Useage $0 [common name]"
- 
-    exit 99
-fi
- 
-echo "Generating key request for $domain"
- 
-#Generate a key
-openssl genrsa -des3 -passout pass:$password -out $domain.key 2048 -noout
- 
-#Remove passphrase from the key. Comment the line out to keep the passphrase
-echo "Removing passphrase from key"
-openssl rsa -in $domain.key -passin pass:$password -out $domain.key
- 
-#Create the request
-echo "Creating CSR"
-openssl req -new -key $domain.key -out $domain.csr -passin pass:$password \
-    -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
- 
-echo "---------------------------"
-echo "-----Below is your CSR-----"
-echo "---------------------------"
-echo
-cat $domain.csr
- 
-echo
-echo "---------------------------"
-echo "-----Below is your Key-----"
-echo "---------------------------"
-echo
-cat $domain.key
+openssl req -new -x509 -nodes -out /etc/openldap/certs/jwadeldapcert.pem -keyout /etc/openldap/certs/jwadeldapkey.pem -days 365<<EOF
+$US \n
+$Washington \n
+$Seattle \n
+$IT \n
+$nti310 \n
+$jwade.local \n
+$jwade005@seattlecentral.edu \n
+EOF

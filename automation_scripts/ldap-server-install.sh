6dd7462643a735362e70618bf313b86fb135bac1
Jonathan <Jonathan@Jonathans-MacBook-Pro.local>
2017-01-25 07:49:08 -0800

    update ldap script

--- automation_scripts/ldap-server-install.sh
+++ automation_scripts/ldap-server-install.sh
@@ -43,14 +43,14 @@ setsebool -P httpd_can_connect_ldap on
 
 #copy db.ldif and add to config
 
-cp /nti-310/config_scripts/db.ldif /etc/openldap/slapd.d/db.ldif
+sudo cp /nti-310/config_scripts/db.ldif /etc/openldap/slapd.d/db.ldif
 
 sudo ldapmodify -Y EXTERNAL  -H ldapi:/// -f db.ldif
 sleep 5
 
 #copy monitor.ldif and add to config
 
-cp /nti-310/config_scripts/monitor.ldif /etc/openldap/slapd.d/monitor.ldif
+sudo cp /nti-310/config_scripts/monitor.ldif /etc/openldap/slapd.d/monitor.ldif
 
 sudo ldapmodify -Y EXTERNAL  -H ldapi:/// -f monitor.ldif
 sleep 5
@@ -80,27 +80,9 @@ sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
 
 #create base.ldif file for domain
 
-cp /nti-310/config_scripts/base.ldif /etc/openldap/slapd.d/base.ldif
+sudo cp /nti-310/config_scripts/base.ldif /etc/openldap/slapd.d/base.ldif
 sudo ldapadd -x -W -D "cn=ldapadm,dc=jwade,dc=local" -f base.ldif
 
-#install tcl tk packages
+#enter ldapadmin password
 
-sudo yum -y install tcl
-sudo yum -y isntall tcl-devel
-
-sudo yum -y install tk
-sudo yum -y install tk-devel
-
-
-#install exdpect package
-
-tar -zxvf expect5.45.tar.gz
-./expect5.45/configure
-
-make
-sudo make install && ln -svf expect5.45/libexpect5.45.so /usr/lib
-
-#build the directory structure
-
-sudo ldapadd -x -W -D "cn=ldapadm,dc=jwade,dc=local" -f base.ldif
-./expect.exp
+echo -e "{SSHA}g4swWPbIesYnD9gF0PlOz1myuGUgogl8" \r

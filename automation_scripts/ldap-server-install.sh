98957811471eed5b5f3eb987307cd52ffedb8f68
Jonathan Wade <jwade005@seattlecentral.edu>
2017-01-25 13:09:55 -0800

    update ldap server test and centos7 script for ldap

--- automation_scripts/ldap-server-install.sh
+++ automation_scripts/ldap-server-install.sh
@@ -4,53 +4,53 @@
 
 #install git
 
-yum -y install git
-git clone https://github.com/jwade005/NTI-310.git
-git config --global user.name "jwade005"
-git config --global user.email "jwade005@seattlecentral.edu"
+sudo yum -y install git
+sudo git clone https://github.com/jwade005/NTI-310.git
+sudo git config --global user.name "jwade005"
+sudo git config --global user.email "jwade005@seattlecentral.edu"
 
 #install ldap
 
-yum -y install openldap-servers openldap-clients
+sudo yum -y install openldap-servers openldap-clients
 
 #copy db config, change ownership
 
-cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
-chown ldap. /var/lib/ldap/DB_CONFIG
+sudo cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
+sudo chown ldap. /var/lib/ldap/DB_CONFIG
 
 #enable and start ldap
 
-systemctl enable slapd
-systemctl start slapd
+sudo systemctl enable slapd
+sudo systemctl start slapd
 
 #install apache
 
-yum -y install httpd
+sudo yum -y install httpd
 
 #enable and start apache
 
-systemctl enable httpd
-systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl start httpd
 
 #install phpldapadmin
 
-yum -y install epel-release
-yum -y install phpldapadmin
+sudo yum -y install epel-release
+sudo yum -y install phpldapadmin
 
 #allow http connection to ldap
 
-setsebool -P httpd_can_connect_ldap on
+sudo setsebool -P httpd_can_connect_ldap on
 
 #copy db.ldif and add to config
 
-sudo cat /nti-310/config_scripts/db.ldif /etc/openldap/slapd.d/db.ldif
+sudo cat /NTI-310/config_scripts/db.ldif /etc/openldap/slapd.d/db.ldif
 
 sudo ldapmodify -Y EXTERNAL  -H ldapi:/// -f db.ldif
 sleep 5
 
 #copy monitor.ldif and add to config
 
-sudo cat /nti-310/config_scripts/monitor.ldif /etc/openldap/slapd.d/monitor.ldif
+sudo cat /NTI-310/config_scripts/monitor.ldif /etc/openldap/slapd.d/monitor.ldif
 sudo chown ldap. /etc/openldap/slapd.d/monitor.ldif
 
 sudo ldapmodify -Y EXTERNAL  -H ldapi:/// -f monitor.ldif
@@ -58,7 +58,7 @@ sleep 5
 
 #create ssl cert
 
-sudo cat /nti-310/config_scripts/create_ldap_ssl.sh /etc/openldap/certs/create_ldap_ssl.sh
+sudo cat /NTI-310/config_scripts/create_ldap_ssl.sh /etc/openldap/certs/create_ldap_ssl.sh
 sudo ./etc/openldap/certs/create_ldap_ssl.sh
 
 echo "Key and Cert created in /etc/openldap/certs"
@@ -70,7 +70,7 @@ sudo ll /etc/openldap/certs/*.pem
 
 #copy cert ldif and add to config
 
-sudo cat /nti-310/config_scripts/certs.ldif /etc/openldap/slapd.d/certs.ldif
+sudo cat /NTI-310/config_scripts/certs.ldif /etc/openldap/slapd.d/certs.ldif
 sudo ldapmodify -Y EXTERNAL  -H ldapi:/// -f certs.ldif
 
 #add the cosine and nis LDAP schemas
@@ -81,14 +81,14 @@ sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
 
 #create base.ldif file for domain
 
-sudo cat /nti-310/config_scripts/base.ldif /etc/openldap/slapd.d/base.ldif
+sudo cat /NTI-310/config_scripts/base.ldif /etc/openldap/slapd.d/base.ldif
 sudo ldapadd -x -W -D "cn=ldapadm,dc=jwade,dc=local" -f base.ldif
 
 #enter ldapadmin password
 
 echo -e "ldapP@ssw0rd1" \r #needs to be encrypted
 
-sudo cat /nti-310/config_scripts/phpldapadmin.conf /etc/httpd/conf.d/phpldapadmin.conf
+sudo cat /NTI-310/config_scripts/phpldapadmin.conf /etc/httpd/conf.d/phpldapadmin.conf
 
 #restart htttpd service
 
@@ -96,5 +96,5 @@ sudo systemctl restart httpd.service
 
 #configure firewall to allow access
 
-firewall-cmd --permanent --zone=public --add-service=http
-firewall-cmd --reload
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --reload

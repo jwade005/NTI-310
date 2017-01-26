e91ce79eb406fb7fd2f7773aee3cf02f1dbf9bbb
Jonathan Wade <jwade005@seattlecentral.edu>
2017-01-25 22:13:24 -0800

    update ldap-server-install.sh

--- automation_scripts/ldap-server-install.sh
+++ automation_scripts/ldap-server-install.sh
@@ -88,7 +88,8 @@ sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
 sudo cp /tmp/NTI-310/config_scripts/base.ldif /etc/openldap/slapd.d/base.ldif
 sudo ldapadd -x -W -D "cn=ldapadm,dc=jwade,dc=local" -f /etc/openldap/slapd.d/base.ldif
 
-echo -e "ldapP@ssw0rd1" \r
+#script stops here and prompts for ldap root password
+
 
 #grants connections from the web
 

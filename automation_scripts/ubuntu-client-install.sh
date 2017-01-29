a24826daf614d78cdd2a3a9ba35f993dd97a09cb
Jonathan Wade <jwade005@seattlecentral.edu>
2017-01-29 09:57:53 -0800

    update ubuntu-client-install.sh

--- automation_scripts/ubuntu-client-install.sh
+++ automation_scripts/ubuntu-client-install.sh
@@ -1,10 +1,21 @@
-#this is a working copy of the ubuntu-ldap-client automation script
+#!/bin/bash
 
-sudo apt-get update
-sudo apt-get install libpam-ldap nscd
+#this is a working copy of the ubuntu client automation script
+#it will be updated throughout the quarter as we add services
+#use with caution
+
+#install gnome-shell
+sudo apt-get --yes install gnome-shell && sudo apt-get --yes install ubuntu-gnome-desktop
+
+#update ubuntu
+sudo apt-get --yes update && sudo apt-get --yes upgrade && sudo apt-get --yes dist-upgrade
+
+#isntall ldap client
+sudo apt-get --yes install libpam-ldap nscd
+
 
 #configure ldap client
-#LDAP server Uniform Resource Identifier: ldap://104.199.116.113
+#LDAP server Uniform Resource Identifier: ldap://104.197.215.83
 
 #Change the initial string from "ldapi:///" to "ldap://" before inputing your server's information
 #Distinguished name of the search base:
@@ -15,23 +26,15 @@ sudo apt-get install libpam-ldap nscd
 
 #LDAP version to use: 3
 
-#Make local root Database admin: Yes
+#Make local root Database admin: No
 
 #Does the LDAP database require login? No
-
-#LDAP account for root:
-
-#This should also match the value in your /etc/phpldapadmin/config.php.
-#Search for: " 'login','bind_id' " within the file
-#cn=ldapadm,dc=jwade,dc=local
-
-#LDAP root account password: Your-LDAP-root-password
 
 #if you make a mistake and need to run the configuration again, use this command
 #sudo dpkg-reconfigure ldap-auth-config
 
 #edit the /etc/nsswitch.conf file - add 'ldap' to these lines
-sudo vi /etc/nsswitch.conf
+sudo vi /etc/nsswitch.conf #---use sed command
 
 #passwd:         ldap compat
 #group:          ldap compat
@@ -39,7 +42,7 @@ sudo vi /etc/nsswitch.conf
 
 
 #edit the PAM config file - /etc/pam.d/common-session
-sudo vi /etc/pam.d/common-session
+sudo vi /etc/pam.d/common-session #---use sed command
 
 #add this line to the bottom of the config file
 #session required    pam_mkhomedir.so skel=/etc/skel umask=0022
@@ -51,10 +54,10 @@ sudo /etc/init.d/nscd restart
 sudo visudo
 
 #comment out this line
-#%admin ALL=(ALL) ALL
+#%admin ALL=(ALL) ALL #---use sed command
 
 #adjust the ssh config file for the ubuntu-desktop instance /etc/ssh/sshd_config
-sudo vi /etc/ssh/sshd_config
+sudo vi /etc/ssh/sshd_config #---use sed command
 #comment out these two lines
 #PasswordAuthentication no
 #ChallengeResponseAuthentication no
@@ -63,5 +66,5 @@ sudo vi /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
 
 #login as ldap user on the ubuntu-desktop!
-
-
+#command from terminal: ssh <username>@<ubuntuIPaddress>
+#enter user password defined in phpldapadmin

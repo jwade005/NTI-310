e22962d8114e7643e6eb801273750f543e1c9b7c
Jonathan Wade <jwade005@seattlecentral.edu>
2017-01-30 17:49:06 -0800

    update automation scripts

--- automation_scripts/nfs-server-install.sh
+++ automation_scripts/nfs-server-install.sh
@@ -25,9 +25,18 @@ sudo chmod 755 /var/config
 #adjust /etc/exports to allow sharing of folders ***must use internal IPs***
 #sudo vi /etc/exports
 #add these lines       ***use sed--add uncommented lines-empty file***
-sudo sed '1 a\/home           10.128.0.3(rw,sync,no_root_squash,no_subtree_check)' /etc/exports
-sudo sed '2 a\/var/dev        10.128.0.3(rw,sync,no_subtree_check)' /etc/exports
-sudo sed '3 a\/var/config        10.128.0.3(rw,sync,no_subtree_check)' /etc/exports
+sudo ed -s /etc/exports << 'EOF'
+0a
+/home           10.128.0.3(rw,sync,no_root_squash,no_subtree_check)
+/var/dev        10.128.0.3(rw,sync,no_subtree_check)
+/var/config     10.128.0.3(rw,sync,no_subtree_check)
+.
+w
+EOF
+
+#sudo sed '1 a\/home           10.128.0.3(rw,sync,no_root_squash,no_subtree_check)'\n /etc/exports
+#sudo sed '2 a\/var/dev        10.128.0.3(rw,sync,no_subtree_check)'\n /etc/exports
+#sudo sed '3 a\/var/config     10.128.0.3(rw,sync,no_subtree_check)'\n /etc/exports
 
 #make changes take effect
 sudo exportfs -a
--- config_scripts/create_ldap_ssl.sh
+++ config_scripts/create_ldap_ssl.sh
@@ -1,6 +1,5 @@
 #creates ssl cert without input from user
 
-openssl req -new -x509 -nodes -batch -out /etc/openldap/certs/jwadeldapcert.pem -keyout /etc/openldap/certs/jwadeldapkey.pem -days 3650
+#openssl req -new -x509 -nodes -batch -out /etc/openldap/certs/jwadeldapcert.pem -keyout /etc/openldap/certs/jwadeldapkey.pem -days 3650
 
-
-
+openssl req -new -x509 -nodes -out /etc/openldap/certs/jwadeldapcert.pem -keyout /etc/openldap/certs/jwadeldapkey.pem -days 365 -subj "/C=US/ST=WA/L=Seattle/O=IT/OU=NTI310IT/CN=jwade.local"

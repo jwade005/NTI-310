34ece977d7f837ad9ecac1a11774e854c1823a5b
Jonathan Wade <jwade005@seattlecentral.edu>
2017-02-01 06:31:45 -0800

    Update gcloud-start-centos7.sh

--- automation_scripts/gcloud-start-centos7.sh
+++ automation_scripts/gcloud-start-centos7.sh
@@ -1,4 +1,4 @@
 gcloud compute instances create centos7-ldap-test \
     --image-family centos-7 --image-project centos-cloud \
     --machine-type f1-micro \
-    --zone us-west1-b
+    --zone us-central1-a

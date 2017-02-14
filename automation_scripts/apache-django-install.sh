df7b66caeb98aeaa30c12d7688babf7bdbf276a7
Jonathan Wade <jwade005@seattlecentral.edu>
2017-02-14 11:19:18 -0800

    add notes for django-postgres integration

--- automation_scripts/apache-django-install.sh
+++ automation_scripts/apache-django-install.sh
@@ -134,3 +134,27 @@ python manage.py createsuperuser #<-- will allow admin login
 #python manage.py runserver 0.0.0.0:8000
 #http://server_domain_or_IP:8000/admin
 #
+#
+# (django-env) [Jonathan@django-a-centos7 project1]$ python manage.py migrate
+# Operations to perform:
+#   Apply all migrations: admin, auth, contenttypes, sessions
+# Running migrations:
+#   Applying contenttypes.0001_initial... OK
+#   Applying auth.0001_initial... OK
+#   Applying admin.0001_initial... OK
+#   Applying admin.0002_logentry_remove_auto_add... OK
+#   Applying contenttypes.0002_remove_content_type_name... OK
+#   Applying auth.0002_alter_permission_name_max_length... OK
+#   Applying auth.0003_alter_user_email_max_length... OK
+#   Applying auth.0004_alter_user_username_opts... OK
+#   Applying auth.0005_alter_user_last_login_null... OK
+#   Applying auth.0006_require_contenttypes_0002... OK
+#   Applying auth.0007_alter_validators_add_error_messages... OK
+#   Applying auth.0008_alter_user_username_max_length... OK
+#   Applying sessions.0001_initial... OK
+# (django-env) [Jonathan@django-a-centos7 project1]$ python manage.py createsuperuser
+# Username (leave blank to use 'jonathan'):
+# Email address: jwade005@seattlecentral.edu
+# Password:
+# Password (again):
+# Superuser created successfully.

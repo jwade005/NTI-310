d0ed1786ecb32c5da1daf0bb272d355646eae208
Jonathan Wade <jwade005@seattlecentral.edu>
2017-03-01 17:14:47 -0800

    add postgres.sql for database configuration during install

---
+++ config_scripts/postgres.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE project1;
+CREATE USER project1 WITH PASSWORD 'P@ssw0rd1';
+ALTER ROLE project1 SET client_encoding TO 'utf8';
+ALTER ROLE project1 SET default_transaction_isolation TO 'read committed';
+ALTER ROLE project1 SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE project1 TO project1;

33052d3442c8165566364091ca5e7795ce9dbf1c
Jonathan Wade <jwade005@seattlecentral.edu>
2017-03-06 16:53:00 -0800

    create gcloud automation script

---
+++ automation_scripts/gcloud_automation.sh
@@ -0,0 +1,25 @@
+gcloud auth login jwade005@seattlecentral.edu --no-launch-browser
+
+gcloud alpha projects create nti310-automation-1 \
+    --name="NTI310 Automation 1"
+
+gcloud config configrations activate jwade005_configuration
+
+gcloud config set account jwade005@seattlecental.edu
+
+gcloud config set project nti310-automation-1
+
+gcloud alpha billing accounts projects link nti310-automation-1 --account-id=00CC7B-8C9651-1D73FA
+
+gcloud compute instances create ubuntu-client \
+    --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud \
+    --machine-type f1-micro \
+    --zone us-central1-a \
+    --service-account=jwade005@seattlecentral.edu
+
+gcloud compute instances create ldap-server \
+    rsyslog-server nfs-server postgres-a-test postgres-b-staging postgres-c-production django-a-test django-b-staging django-c-production \
+    --image-family centos-7 --image-project centos-cloud \
+    --machine-type f1-micro \
+    --zone us-central1-a \
+    --service-account=jwade005@seattlecentral.edu
